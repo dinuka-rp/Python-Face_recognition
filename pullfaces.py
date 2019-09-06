f24fa0688374625421e5448744e1a850f457c493
Dinuka <drpiyadigama@gmail.com>
2019-09-06 16:52:30 +0530

    saving identified faces from an image with a group of people, implemented

--- pullfaces.py
+++ pullfaces.py
@@ -1,4 +1,4 @@
-# This program identifies multiple faces in an image an displays them
+# This program identifies multiple faces in an image , displays and saves them
 
 from PIL import Image       #implementing the Pillow library (Imaging library)
 import face_recognition
@@ -12,4 +12,5 @@ for face_location in face_locations:
     face_image = image[top:bottom,left:right]       #gives a face image in a form of an array
 
     pil_image = Image.fromarray(face_image)
-    pil_image.show()
+    # pil_image.show()                      #to display the identified faces in an image
+    pil_image.save(f'{top}.jpg')            #saving the faces identified in an image with the top co-ordinate of the face given as the file name
