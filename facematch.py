569e2658d3930aec0b717e6b18494d1fad996eca
Dinuka <drpiyadigama@gmail.com>
2019-09-06 14:41:26 +0530

    Face match implemented

---
+++ facematch.py
@@ -0,0 +1,15 @@
+import face_recognition
+
+img_of_bill = face_recognition.load_image_file('./img/known/Bill_Gates.jpg')
+bill_face_encoding = face_recognition.face_encodings(img_of_bill)[0]        #face_encodings return an array. Only the first item of the array is considered
+
+unkown_img = face_recognition.load_image_file('./img/unknown/steve1.jpg')
+unkown_encoding = face_recognition.face_encodings(unkown_img)[0]        #face_encodings return an array. Only the first item of the array is considered
+
+#comparing the 2 faces in the 2 images
+results = face_recognition.compare_faces([bill_face_encoding],unkown_encoding)    #comparing one encoding each
+
+if results[0]:
+    print("This is Bill Gates")
+else:
+    print("This is NOT Bill Gates")
--- findfaces.py
+++ findfaces.py
@@ -2,6 +2,6 @@ import face_recognition
 
 image = face_recognition.load_image_file('./img/groups/team.jpg')
 face_locations = face_recognition.face_locations(image)     #get locations of faces in image
-#gives an array of co-ordinates of each face in the image
 
-print(face_locations)
+print(face_locations)       #gives an array of co-ordinates of each face in the image
+print(f'There are {len(face_locations)} people in this image')         #identifying the number of people in an image
